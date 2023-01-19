3f6cdd3bd83d909d65df3077988cb75f5f575761
Janus Asmussen <jjasmussen@outlook.com>
2023-01-19 16:56:38 +0100

    Add test

--- test/test_common_functions.py
+++ test/test_common_functions.py
@@ -2,16 +2,23 @@ import os
 import pytest
 from collections import namedtuple
 
+from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
 
 from spark_utils.models.job_socket import JobSocket
-from spark_utils.common.functions import read_from_socket
+from spark_utils.common.functions import read_from_socket, write_to_socket
 
-Format = namedtuple('format', ['format', 'spark_options'])
+
+def are_dfs_equal(df1: DataFrame, df2: DataFrame) -> bool:
+    """Asserts if dataframes are equal"""
+    return (df1.schema == df2.schema) and (df1.collect() == df2.collect())
+
+
+Format = namedtuple('format', ['format', 'read_options'])
 @pytest.mark.parametrize('format_', [
-    Format(format='csv', spark_options={'header': True}),
-    Format(format='json', spark_options={}),
-    Format(format='parquet', spark_options={}),
+    Format(format='csv', read_options={'header': True}),
+    Format(format='json', read_options={}),
+    Format(format='parquet', read_options={}),
 ])
 def test_read_from_socket(format_: Format, spark_session: SparkSession, test_base_path: str):
 
@@ -22,9 +29,37 @@ def test_read_from_socket(format_: Format, spark_session: SparkSession, test_bas
         data_path=f'file:///{os.path.join(test_data_path, f"data.{format_.format}")}',
         data_format=format_.format,
     )
-    df = read_from_socket(socket=socket, spark_session=spark_session, read_options=format_.spark_options)
+    df = read_from_socket(socket=socket, spark_session=spark_session, read_options=format_.read_options)
 
     assert sorted(df.columns) == sorted(['strings', 'ints', 'floats'])
+
+
+Format = namedtuple('format', ['format', 'read_options'])
+@pytest.mark.parametrize('format_', [
+    Format(format='csv', read_options={'header': True}),
+    Format(format='json', read_options={}),
+    Format(format='parquet', read_options={}),
+])
+def test_write_to_socket(format_: Format, spark_session: SparkSession, test_base_path: str):
+
+    test_data_path = os.path.join(test_base_path, 'test_common_functions')
+    socket = JobSocket(
+        alias='test',
+        data_path=f'file:///{os.path.join(test_data_path, f"data.{format_.format}")}',
+        data_format=format_.format,
+    )
+    output_socket = JobSocket(
+        alias='test',
+        data_path=f'file:///{os.path.join(test_data_path, "write", f"data.{format_.format}")}',
+        data_format=format_.format,
+    )
+    df = read_from_socket(socket=socket, spark_session=spark_session, read_options=format_.read_options)
+
+    write_to_socket(data=df, socket=output_socket, write_options=format_.read_options, )
+
+    df_read = read_from_socket(socket=output_socket, spark_session=spark_session, read_options=format_.read_options)
+
+    assert are_dfs_equal(df, df_read)
 
 
 @pytest.mark.parametrize('sep', ['|', ';'])
