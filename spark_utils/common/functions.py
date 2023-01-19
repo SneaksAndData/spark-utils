f0931917ffe689904699ee5a09282c00497fcc18
Janus Asmussen <jjasmussen@outlook.com>
2023-01-19 12:30:47 +0100

    Resolve GZU comments

--- spark_utils/common/functions.py
+++ spark_utils/common/functions.py
@@ -24,7 +24,7 @@
   Common helper functions.
 """
 import os
-from typing import Optional, List
+from typing import Optional, List, Dict
 
 from cryptography.fernet import Fernet
 from pyspark.sql import SparkSession, DataFrame
@@ -62,20 +62,24 @@ def decrypt_sensitive(sensitive_content: Optional[str]) -> Optional[str]:
     return None
 
 
-def read_from_socket(socket: JobSocket, spark_session: SparkSession, **kwargs) -> DataFrame:
+def read_from_socket(
+    socket: JobSocket,
+    spark_session: SparkSession,
+    read_options: Optional[Dict[str, str]] = None,
+) -> DataFrame:
     """Reads data with location specified by socket. kwargs are passed to spark options
 
     :param socket: Socket
     :param spark_session: Spark session
-    :param kwargs: Keyword arguments passed to spark read options
+    :param read_options:
     :return: Spark dataframe
     """
-    if socket.data_format == 'hive':
+    if socket.data_format.startswith('hive'):
         return spark_session.table(socket.data_path)
 
     return spark_session \
         .read \
-        .options(**kwargs) \
+        .options(**read_options) \
         .format(socket.data_format) \
         .load(socket.data_path)
 
@@ -83,33 +87,36 @@ def read_from_socket(socket: JobSocket, spark_session: SparkSession, **kwargs) -
 def write_to_socket(
     data: DataFrame,
     socket: JobSocket,
-    partition_by: List[str] = None,
-    partition_count: int = None,
-    **kwargs
+    partition_by: Optional[List[str]] = None,
+    partition_count: Optional[int] = None,
+    write_options: Optional[Dict[str, str]] = None,
 ) -> None:
     """Writes data to socket
 
     :param data: Dataframe to write
     :param socket: Socket to write to
     :param partition_by: List of column names to partition by
-    :param partition_count: Number of partitions to use
-    :param kwargs: Keyword arguments passed to spark write options
+    :param partition_count: Number of partitions to split result into.
+    :param write_options: Write options passed to spark (e.g. Parquet options
+    found here: https://spark.apache.org/docs/latest/sql-data-sources-parquet.html#data-source-option)
     """
-    partition_count = partition_count or 200
-    partition_by = partition_by or []
-    writer = data \
-        .repartition(partition_count, *partition_by) \
-        .write \
+    if partition_count:
+        data = data.repartition(partition_count, *partition_by)
+
+    writer = data.write \
         .mode('overwrite') \
-        .options(**kwargs)
+        .options(**write_options)
+
+    if partition_by:
+        writer = writer.partitionBy(*partition_by)
 
     if socket.data_format in ['delta', 'parquet']:
         writer \
             .format(socket.data_format) \
             .save(socket.data_path)
-    elif socket.data_format in ['hive']:
+    elif socket.data_format.startswith('hive'):
         writer \
-            .format('delta') \
+            .format(socket.data_format.split('_')[-1]) \
             .saveAsTable(socket.data_path)
     else:
         raise ValueError(f'Output socket data format {socket.data_format} not supported!')
