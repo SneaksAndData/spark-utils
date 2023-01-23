bf2428546ee9e097cef3db41c46c4b3022074e43
JanusAsmussen <46710143+JanusAsmussen@users.noreply.github.com>
2023-01-23 15:01:34 +0100

    Add write_to_socket function and support loading of hive tables in read_from_socket function (#88)
    * Add write_to_socket function and support loading of hive tables in read_from_socket function
    
    * Remove redundant option
    
    * Resolve GZU comments
    
    * Fix test
    
    * Minor refactoring & addition of test
    
    * Add test

--- spark_utils/common/functions.py
+++ spark_utils/common/functions.py
@@ -24,7 +24,7 @@
   Common helper functions.
 """
 import os
-from typing import Optional
+from typing import Optional, List, Dict
 
 from cryptography.fernet import Fernet
 from pyspark.sql import SparkSession, DataFrame
@@ -62,16 +62,62 @@ def decrypt_sensitive(sensitive_content: Optional[str]) -> Optional[str]:
     return None
 
 
-def read_from_socket(socket: JobSocket, spark_session: SparkSession, **kwargs) -> DataFrame:
+def read_from_socket(
+    socket: JobSocket,
+    spark_session: SparkSession,
+    read_options: Optional[Dict[str, str]] = None,
+) -> DataFrame:
     """Reads data with location specified by socket. kwargs are passed to spark options
 
     :param socket: Socket
     :param spark_session: Spark session
-    :param kwargs: Keyword arguments passed to spark read options
+    :param read_options: Read options passed to spark (e.g. Parquet options
+    found here: https://spark.apache.org/docs/latest/sql-data-sources-parquet.html#data-source-option)
     :return: Spark dataframe
     """
+    read_options = read_options or {}
+    if socket.data_format.startswith('hive'):
+        return spark_session.table(socket.data_path)
+
     return spark_session \
         .read \
-        .options(**kwargs) \
+        .options(**read_options) \
         .format(socket.data_format) \
         .load(socket.data_path)
+
+
+def write_to_socket(
+    data: DataFrame,
+    socket: JobSocket,
+    partition_by: Optional[List[str]] = None,
+    partition_count: Optional[int] = None,
+    write_options: Optional[Dict[str, str]] = None,
+) -> None:
+    """Writes data to socket
+
+    :param data: Dataframe to write
+    :param socket: Socket to write to
+    :param partition_by: List of column names to partition by
+    :param partition_count: Number of partitions to split result into.
+    :param write_options: Write options passed to spark (e.g. Parquet options
+    found here: https://spark.apache.org/docs/latest/sql-data-sources-parquet.html#data-source-option)
+    """
+    write_options = write_options or {}
+    if partition_count:
+        data = data.repartition(partition_count, *partition_by)
+
+    writer = data.write \
+        .mode('overwrite') \
+        .options(**write_options)
+
+    if partition_by:
+        writer = writer.partitionBy(*partition_by)
+
+    if socket.data_format.startswith('hive'):
+        writer \
+            .format(socket.data_format.split('_')[-1]) \
+            .saveAsTable(socket.data_path)
+    else:
+        writer \
+            .format(socket.data_format) \
+            .save(socket.data_path)
