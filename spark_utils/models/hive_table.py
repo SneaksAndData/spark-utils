d062db9d40cddf8d29fa5ed6a0e1ef170e54b881
George Zubrienko <gzu@ecco.com>
2025-12-17 14:04:35 +0100

    WIP refactor delta

--- spark_utils/models/hive_table.py
+++ spark_utils/models/hive_table.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2022 Ecco Sneaks & Data
+# Copyright (c) 2022-2026 Ecco Sneaks & Data
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
--- spark_utils/models/iceberg_rest_config.py
+++ spark_utils/models/iceberg_rest_config.py
@@ -20,40 +20,33 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-"""
-  Options for copy_data.py.
-"""
 
 from dataclasses import dataclass
-from typing import Optional
-
-from spark_utils.models.job_socket import JobSocket
 
 
 @dataclass
-class CopyDataOptions:
+class IcebergRestConfig:
     """
-      Options for copy_data.py
-
-    :arg src: Source job socket
-    :arg dest: Destination job socket
-    :arg read_options: Spark session options to set when reading
-    :arg write_options: Spark session options to set when writing
-    :arg include_filename: Adds "filename" column to the destination output.
-    :arg include_row_sequence: Adds "sequence_number" column to the destination output.
-    :arg clean_destination: Wipe destination path before starting a copy.
-    :arg timestamp_column: Column name to use for evaluating data age.
-    :arg timestamp_column_format: Format for the timestamp
+    Iceberg configs for Spark session
     """
 
-    src: JobSocket
-    dest: JobSocket
-    include_filename: bool = False
-    include_row_sequence: bool = False
-    clean_destination: bool = False
-    clean_column_names: bool = False
-    read_options: Optional[dict] = None
-    write_options: Optional[dict] = None
-    output_file_count: Optional[int] = None
-    timestamp_column: Optional[str] = None
-    timestamp_column_format: Optional[str] = None
+    catalog_uri: str
+    oauth2_uri: str
+    warehouse: str
+    scope: str
+    client_id: str
+    client_secret: str
+
+    version: str = "org.apache.iceberg:iceberg-spark-runtime-3.5_2.13:1.10.0"
+    s3_version: str = "org.apache.iceberg:iceberg-aws-bundle:1.10.0"
+    catalog_alias: str = "iceberg_rest_default"
+    catalog_class: str = "org.apache.iceberg.spark.SparkCatalog"
+    catalog_type: str = "rest"
+    sql_extensions: str = "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions"
+    catalog_impl: str = "org.apache.iceberg.rest.RESTCatalog"
+
+    def get_credentials(self) -> str:
+        """
+        Generate Iceberg REST credential
+        """
+        return f"{self.client_id}:{self.client_secret}"
